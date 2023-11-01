e74b0494d03c01ac018fdfe28112b388abdc67f4
Jalal Ouraigua <ouraigua@icloud.com>
2023-11-01 22:22:39 +0100

    Updated JenkinsFile with a forloop

--- vars/integrationArtifactUpload.groovy
+++ vars/integrationArtifactUpload.groovy
@@ -10,7 +10,7 @@ void call(Map parameters = [:]) {
 
     // Access the 'integrationFlowId' parameter from the 'parameters' map
     String integrationFlowId = parameters.integrationFlowId ?: 'TestFlow'
-    String filePath = parameters.filePath ?: 'TestFlow.zip'
+    String savePath = parameters.savePath ?: 'TestFlow.zip'
 
     piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
 }
